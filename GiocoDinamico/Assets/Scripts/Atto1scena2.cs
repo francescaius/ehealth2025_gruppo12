a32e10732ba5f243f38b9577cf4428bafe980006
DanieleMorcone <124515852+DanieleMorcone@users.noreply.github.com>
2025-11-21 17:27:09 +0100

    Atto1Scena1finitomaerrori

--- GiocoDinamico/Assets/Scripts/Atto1scena2.cs
+++ GiocoDinamico/Assets/Scripts/Atto1scena2.cs
@@ -32,25 +32,48 @@ public class Atto1scena2 : MonoBehaviour
 
         // yield return personaggio.Appear();
         yield return new WaitForSeconds(1);
-        yield return Luca.Appear();
-        yield return VisualNovelManager.S.dialog.DisplayText(
-            "Luca",
-            "C'è qualcuno??"
-        );
         yield return Marta.Appear();
         yield return VisualNovelManager.S.dialog.DisplayText(
             "Marta",
-            "Eccomi"
+            "Da quanto tempo! Cosa posso prepararti?"
         );        
-
-        yield return Luca.ChangePose("Preoccupato"); 
+        yield return Luca.Appear();
         yield return VisualNovelManager.S.dialog.DisplayText(
             "Luca",
-            "Mi hai spaventato"
+            "Non ho molta fame... un semplice caffè, grazie"
+        );    
+        yield return VisualNovelManager.S.dialog.DisplayText(
+            "Marta",
+            "Posso aiutarti? Sembri perso..."
+        );  
+        yield return Luca.ChangePose("Triste");
+        yield return VisualNovelManager.S.dialog.DisplayText(
+            "Luca",
+            "Cercavo qualcuno che conosci bene"
+        );   
+        yield return Marta.ChangePose("Preoccupata");
+        yield return VisualNovelManager.S.dialog.DisplayText(
+            "Marta",
+            "Dimmi pure di chi si tratta"
+        );        
+        yield return VisualNovelManager.S.dialog.DisplayText(
+            "Luca",
+            "Mio fratello, so che veniva spesso qui"
+        );  
+        yield return Marta.ChangePose("Pensierosa");
+        yield return VisualNovelManager.S.dialog.DisplayText(
+            "Marta",
+            "Non ho notizie di lui da un po'. Lasciò qualcosa ma non so se dovrei dartelo..."
         );
         yield return Anonimo.Appear();
+        yield return Luca.ChangePose("Preoccupato");
+        yield return VisualNovelManager.S.dialog.DisplayText(
+            "Luca",
+            "Di nuovo quella notifica. Stesso tono, stesso mittente. E se fosse lui? E se fosse mio fratello? Apro la notifica o continuo a parlare con Marta?"
+        );
         Anonimo.MakeClickable (LeggiNotifica);
-        Marta.MakeClickable (Parla);
+        Marta.MakeClickable(Parla);
+      
 
 
         //yield return VisualNovelManager.S.Element("Overlay").Appear();
@@ -100,17 +123,65 @@ public class Atto1scena2 : MonoBehaviour
         yield return Anonimo.Disappear ();
         yield return VisualNovelManager.S.dialog.DisplayText(
             "Anonimo",
-            "Contenuto Notifica"
+            "Non fidarti di nessuno. Esci ora. Ti guiderò io."
         );
-        Marta.MakeClickable(Parla);
+        
+        yield return Marta.ChangePose("Arrabiata");
+        yield return VisualNovelManager.S.dialog.DisplayText(
+            "Marta",
+            "Mi stai ascoltando??"
+        );
+       yield return VisualNovelManager.S.dialog.DisplayText(
+            "Luca",
+            "Aspetta... che significa? Chi sei tu?"
+
+        );
+        yield return Marta.ChangePose("Preoccupata");
+        yield return VisualNovelManager.S.dialog.DisplayText(
+            "Marta",
+            "Ma cosa dici? Parli con me?"
+        );
+         yield return Luca.ChangePose("Triste");
+        yield return VisualNovelManager.S.dialog.DisplayText(
+            "Luca",
+            "No scusa, sto cercando di capire... qualcuno mi sta aiutando..."
+        );
+        yield return Marta.ChangePose("Arrabiata");
+        yield return VisualNovelManager.S.dialog.DisplayText(
+            "Marta",
+            "Aiutando? O controllando? Da quando sei entrato non hai mai smesso di guardare quel telefono"
+        );
+        yield return Luca.ChangePose("Preoccupato");
+        yield return VisualNovelManager.S.dialog.DisplayText(
+            "Luca",
+            "Non capisci… è importante. Forse è lui. Mio fratello."
+        );
+        yield return VisualNovelManager.S.dialog.DisplayText(
+        "Marta",
+        "Tuo fratello veniva qui per staccare dal mondo, non per affondarci dentro. Ma vedo che siete così diversi"
+        );
+        yield return Luca.Disappear (); //luca esce dal bar perchè l'utente ha deciso di cliccare la notifica
+        // caso in cui l'utente sceglie di cliccare su marta ed ignorare la notifica
+        yield return Luca.ChangePose("Rilassato");
+        yield return VisualNovelManager.S.dialog.DisplayText(
+            "Luca",
+            "Aspetta. Forse questo ti farà capire che non mento"
+
+        );
+        yield return Marta.ChangePose("Rilassata");
+        yield return VisualNovelManager.S.dialog.DisplayText(
+            "Marta",
+            "Questa foto me la fece vedere tuo fratello e mi disse che, se mai qualcuno me l'avesse mostrata, dovevo dargli questo"
+        );
     }
-    
-    public void Parla()
+        {
+            StartCoroutine (Scelta2());
+        } 
+        
     {
-        StartCoroutine (Scelta2());
-          
-    }
+    
     private IEnumerator Scelta2 ()
+    
     {
         Marta.UndoClickable();
         Anonimo.UndoClickable();
@@ -124,8 +195,7 @@ public class Atto1scena2 : MonoBehaviour
         yield return VisualNovelManager.S.Element("PuzzlePiece").Disappear();
         yield return VisualNovelManager.S.Element("Overlay").Appear();
     }
-
     
-
     
+     
 }
