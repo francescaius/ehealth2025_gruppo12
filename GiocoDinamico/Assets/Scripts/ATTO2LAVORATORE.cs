7bc10e5727a3f1cc4c37adeb921f0e7fadd9dcb0
Frasardu <francesco.sardu@mail.polimi.it>
2025-12-13 13:02:44 +0100

    ATTO5TEMPIO

--- GiocoDinamico/Assets/Scripts/ATTO2LAVORATORE.cs
+++ GiocoDinamico/Assets/Scripts/ATTO2LAVORATORE.cs
@@ -98,12 +98,12 @@ public class ATTO2LAVORATORE : MonoBehaviour
         }
         else {
         yield return Mattia.ChangePose("ridendo");
-        yield return VisualNovelManager.S.dialog.DisplayText ("Mattia", "You kept thinking about that job oﬀer?");
+        yield return VisualNovelManager.S.dialog.DisplayText ("Mattia", "You kept thinking about that job offer?");
         yield return Luca.ChangePose("incuriosito");
         yield return VisualNovelManager.S.dialog.DisplayText ("Luca","Yeah, a bit. You know how it is… with my degree, master’s, and that project I carried out last year, I should be the perfect candidate, right? I’ve been chasing results for years… and in the end I did build something");
         yield return Mattia.ChangePose ("parlando");
         yield return VisualNovelManager.S.dialog.DisplayText ("Mattia","Yeah, you never stopped. But you seem destroyed, not satisfied.");
-        yield return VisualNovelManager.S.dialog.DisplayText ("Luca","Eﬀort shows you’re growing, right? The late nights, the competition, the courses… everything leads somewhere. In the end… I can say I made it. I did everything I had to.");
+        yield return VisualNovelManager.S.dialog.DisplayText ("Luca","Effort shows you’re growing, right? The late nights, the competition, the courses… everything leads somewhere. In the end… I can say I made it. I did everything I had to.");
         yield return Mattia.ChangePose ("perplesso");
         yield return VisualNovelManager.S.dialog.DisplayText ("Mattia","Yet you don’t seem happy.");
         yield return VisualNovelManager.S.dialog.DisplayText ("Luca","Happiness is overrated. What matters is moving forward, accumulating results, keeping up. In our field if you stop, you’re dead. Luckily I… never stop.");
@@ -113,7 +113,7 @@ public class ATTO2LAVORATORE : MonoBehaviour
         }
         
         yield return Mattia.ChangePose("parlando");
-        yield return VisualNovelManager.S.dialog.DisplayText ("Mattia", "Anyway, I was looking for you. They were talking about the closure of that library we used to go to as kids… and about some guy who drew on the wall right before they shut it down. Tons of graﬃti. Strange, huh?");
+        yield return VisualNovelManager.S.dialog.DisplayText ("Mattia", "Anyway, I was looking for you. They were talking about the closure of that library we used to go to as kids… and about some guy who drew on the wall right before they shut it down. Tons of graffti. Strange, huh?");
         yield return Luca.ChangePose ("incuriosito");
         yield return VisualNovelManager.S.dialog.DisplayText ("Luca", "Actually...");
         yield return VisualNovelManager.S.dialog.DisplayText ("Mattia","Yeah, some say he’s crazy, others say he’s an artist. I still don’t know. There was even a photo,look.");
@@ -124,7 +124,7 @@ public class ATTO2LAVORATORE : MonoBehaviour
         yield return Mattia.Disappear();
         yield return VisualNovelManager.S.Element("Overlay").Disappear();
         yield return VisualNovelManager.S.dialog.DisplayText ("Mattia","This. It’s the old library, the one near the station.");
-        yield return VisualNovelManager.S.dialog.DisplayText ("Luca","I don’t know why… it reminds me of my brother. Could be a coincidence. Just a random graﬃti.");
+        yield return VisualNovelManager.S.dialog.DisplayText ("Luca","I don’t know why… it reminds me of my brother. Could be a coincidence. Just a random graffiti.");
 
         yield return VisualNovelManager.S.Element("Overlay").Appear();
         yield return background.ChangePose("ufficio sfocato");
@@ -161,7 +161,7 @@ public class ATTO2LAVORATORE : MonoBehaviour
         { yield return VisualNovelManager.S.dialog.DisplayText ("Mattia", "you can’t find every answer on a phone.");
         }
         
-        yield return VisualNovelManager.S.dialog.DisplayText ("Mattia","That graﬃti is real. It’s there, not on a screen.");
+        yield return VisualNovelManager.S.dialog.DisplayText ("Mattia","That graffiti is real. It’s there, not on a screen.");
         yield return VisualNovelManager.S.dialog.DisplayText ("Luca","Yeah, but someone must have posted something. Maybe a report… an old post… I just need to… keep searching.");
         
         if (VisualNovelManager.S.ForteDipendenza)
